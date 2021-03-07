6e44bde9783d8b082bbade0b7f8bbef387f391cd
zastrixarundell <zastrixarundell@gmail.com>
2021-03-08 00:04:33 +0100

    Starting with audio

--- lib/spoticord/commands/join.ex
+++ lib/spoticord/commands/join.ex
@@ -7,11 +7,11 @@ defmodule Spoticord.Commands.Join do
   def caller, do: "join"
   def desc, do: "Join the current voice channel!"
 
-  def execute(message, _args) do
+  def execute(message, args) do
     case Utils.get_sender_voice_id(message) do
       {:ok, {guild_id, voice_id}} ->
         if Utils.can_connect?(guild_id, voice_id) do
-          Utils.join_or_switch_voice(guild_id, voice_id)
+          handle_voice(guild_id, voice_id, message, args)
         else
           Nostrum.Api.create_message!(message.channel_id, embed: no_perms_embed())
         end
@@ -22,6 +22,22 @@ defmodule Spoticord.Commands.Join do
   end
 
   alias Nostrum.Struct.Embed
+
+  def handle_voice(guild_id, voice_id, _message, _args) do
+    Utils.join_or_switch_voice(guild_id, voice_id)
+    play_when_ready("https://www.youtube.com/watch?v=R5U98v1yxd0", guild_id)
+  end
+
+  # Recurison ___MAGIC___.
+  defp play_when_ready(link, guild_id) do
+    if Nostrum.Voice.ready?(guild_id) do
+      Nostrum.Voice.play(guild_id, link, :ytdl)
+      |> IO.inspect(label: "Voice")
+    else
+      :timer.sleep(10)
+      play_when_ready(link, guild_id)
+    end
+  end
 
   def no_channel_embed() do
     Utils.create_empty_embed!()
