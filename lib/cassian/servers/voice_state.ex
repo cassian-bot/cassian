500451531e3fc0cd599a915e1978809090ea9771
zastrixarundell <zastrixarundell@gmail.com>
2021-03-10 19:51:41 +0100

    Working VoiceState server model

--- lib/cassian/servers/voice_state.ex
+++ lib/cassian/servers/voice_state.ex
@@ -3,6 +3,12 @@ defmodule Cassian.Servers.VoiceState do
   A GenServer representing the current voice state.
   """
 
+  alias Cassian.Structs.VoiceState
+
+  require Logger
+
+  @timeout 5_000
+
   use GenServer
 
   @doc """
@@ -14,7 +20,7 @@ defmodule Cassian.Servers.VoiceState do
   end
 
   @doc """
-  Delete the queue.
+  Delete the VoiceState.
   """
   @spec delete(guild_id :: Snowflake.t()) :: :ok
   def delete(guild_id) do
@@ -22,18 +28,69 @@ defmodule Cassian.Servers.VoiceState do
     :ok
   end
 
+  @doc """
+  Get the `VoiceState` from voice. Returns an existing or creates and stores in the GenServer.
+  """
+  @spec get!(guild_id :: Snowflake.t()) :: %VoiceState{}
+  def get!(guild_id) do
+    if exists?(guild_id) do
+      GenServer.call(from_guild_id(guild_id), {:get})
+    else
+      state = VoiceState.create!(guild_id)
+      start(guild_id, state)
+      state
+    end
+  end
+
+  @doc """
+  Put the `VoiceState`.
+  """
+  @spec put(state :: %VoiceState{}) :: :ok
+  def put(state) do
+    GenServer.cast(from_guild_id(state.guild_id), {:put, state})
+  end
+
   # Private API
 
   @doc false
-  def start(guild_id, links) do
-    GenServer.start_link(__MODULE__, links, name: from_guild_id(guild_id))
+  def handle_call({:get}, _from, state) do
+    {:reply, state, state, @timeout}
   end
 
   @doc false
-  def init(args) do
-    {:ok, args}
+  def handle_cast({:put, new}, _state) do
+    Logger.debug("Putting new value #{Poison.encode!(new)}")
+    {:reply, new, new, @timeout}
   end
 
   @doc false
-  defp from_guild_id(guild_id), do: "state_#{guild_id}" |> String.to_atom()
+  def handle_info(:timeout, %VoiceState{status: :noop} = state) do
+    Logger.debug("Matched #{Poison.encode!(state)}")
+    {:stop, {:shutdown, :afk}, state}
+  end
+
+  @doc false
+  def handle_info(:timeout, state) do
+    Logger.debug("Not matched #{Poison.encode!(state)}")
+    {:noreply, state, @timeout}
+  end
+
+  @doc false
+  def init(state) do
+    {:ok, state, @timeout}
+  end
+
+  def terminate({:shutdown, :afk}, state) do
+    Logger.debug("Clossing #{Poison.encode!(state)}")
+    Cassian.Utils.Voice.join_or_switch_voice(state.guild_id, nil)
+    {:shutdown, :afk}
+  end
+
+  @doc false
+  def start(guild_id, state) do
+    GenServer.start(__MODULE__, state, name: from_guild_id(guild_id))
+  end
+
+  @doc false
+  defp from_guild_id(guild_id), do: "voice_state_#{guild_id}" |> String.to_atom()
 end
