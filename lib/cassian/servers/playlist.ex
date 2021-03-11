089c5fcdae412e095fda5552d3a784146c4ad7d2
zastrixarundell <zastrixarundell@gmail.com>
2021-03-11 22:44:05 +0100

    Updated playlist genserver for shuffling and sorting

--- lib/cassian/servers/playlist.ex
+++ lib/cassian/servers/playlist.ex
@@ -16,52 +16,29 @@ defmodule Cassian.Servers.Playlist do
   end
 
   @doc """
-  Insert metadata into a playlist, won't work if the playlist doesn't
-  exist.
+  Safely return the current playlist.
   """
-  @spec insert(guild_id :: Snowflake.t(), metadata :: %Metadata{}) :: :ok
-  def insert(guild_id, metadata) do
-    GenServer.cast(from_guild_id(guild_id), {:insert, {metadata, nil}})
+  @spec show(guild_id :: Snowflake.t()) :: {:ok, %Playlist{}} | {:error, :noop}
+  def show(guild_id) do
+    if exists?(guild_id),
+    do: {:ok, GenServer.call(from_guild_id(guild_id), :show)},
+    else: {:error, :noop}
   end
 
   @doc """
-  Insert a metdata into a playlist. If the queue doesn't exist, start it.
+  Insert a metdata into a playlist. If the playlist doesn't exist, start it.
   """
   @spec insert!(guild_id :: Snowflake.t(), metadata :: %Metadata{}) :: :ok
   def insert!(guild_id, metadata) do
     unless exists?(guild_id) do
       start(guild_id, metadata)
     else
-      insert(guild_id, metadata)
+      GenServer.cast(from_guild_id(guild_id), {:insert, metadata})
     end
   end
 
   @doc """
-  Pop a metadata out of the queue.
-  """
-  @spec pop!(guild_id :: Snowflake.t()) :: %Metadata{}
-  def pop!(guild_id) do
-    GenServer.call(from_guild_id(guild_id), {:pop})
-  end
-
-  @doc """
-  Remove a metadata out of the queue.
-  """
-  @spec remove!(guild_id :: Snowflake.t(), metadata :: %Metadata{}) :: :ok
-  def remove!(guild_id, link) do
-    GenServer.cast(from_guild_id(guild_id), {:remove, [link]})
-  end
-
-  @doc """
-  Show all of the metadatas in the queue.
-  """
-  @spec show(guild_id :: Snowflake.t()) :: list(%Metadata{})
-  def show(guild_id) do
-    GenServer.call(from_guild_id(guild_id), {:show})
-  end
-
-  @doc """
-  Delete the queue.
+  Delete the playlist. Ignores if it doesn't exist
   """
   @spec delete(guild_id :: Snowflake.t()) :: :ok
   def delete(guild_id) do
@@ -70,41 +47,141 @@ defmodule Cassian.Servers.Playlist do
   end
 
   @doc """
-  Check whether the queue is empty.
+  Shuffle the playlist. Returns `:error` if the playlist doesn't exist.
   """
-  @spec empty?(guild_id :: Snowflake.t()) :: boolean()
-  def empty?(guild_id) do
-    Enum.empty?(show(guild_id))
+  @spec shuffle(guild_id :: Snowflake.t()) :: :ok | :error
+  def shuffle(guild_id) do
+    if exists?(guild_id) do
+      GenServer.cast(from_guild_id(guild_id), :shuffle)
+      :ok
+    else
+      :error
+    end
+  end
+
+  @doc """
+  Unshuffle the playlist. Returns `:error` if the playlist doesn't exist.
+  """
+  @spec unshuffle(guild_id :: Snowflake.t()) :: :ok | :error
+  def unshuffle(guild_id) do
+    if exists?(guild_id) do
+      GenServer.cast(from_guild_id(guild_id), :unshuffle)
+      :ok
+    else
+      :error
+    end
+  end
+
+  @doc """
+  Get metas with their index. The returned list is sorted depending on the current
+  playlist options.
+  """
+  @spec get_ordered_playlist(guild_id :: Snowflake.t()) :: {:ok, {integer(), list(%Metadata{})}} | {:error, :noop}
+  def get_ordered_playlist(guild_id) do
+    case show(guild_id) do
+      {:error, :noop} ->
+        {:error, :noop}
+
+      {:ok, playlist} ->
+        {:ok, indexed_sorted(playlist)}
+    end
+  end
+
+  @doc false
+  defp indexed_sorted(playlist) do
+    if playlist.shuffle do
+      {playlist.index, shuffle_sort(playlist)}
+    else
+      {playlist.index, extract_metadatas_ordered(playlist.elements)}
+    end
+  end
+
+  @doc """
+  Sort the playlist to how it should play when it is being shuffled.
+  """
+  @spec shuffle_sort(playlist :: %Playlist{}) :: list(%Metadata{})
+  def shuffle_sort(playlist) do
+    Enum.sort_by(playlist.elements, fn {_metadata, index} -> index end)
+    |> extract_metadatas_ordered()
+  end
+
+  @doc """
+  Extract the metadatas from an already-ordered list.
+  """
+  def extract_metadatas_ordered(elements) do
+    Enum.reduce(elements, [], fn {metadata, _index}, acc -> acc ++ [metadata] end)
   end
 
   # Private API
 
   @doc false
-  def handle_call({:show}, _from, state) do
+  def handle_call(:show, _from, state) do
     {:reply, state, state}
   end
 
   @doc false
-  def handle_call({:pop}, _from, state) do
-    {metadata, state} = List.pop_at(state, 0)
-    {:reply, metadata, state}
-  end
-
-  @doc false
-  def handle_cast({:remove, metadata}, state) do
-    state = state -- [metadata]
-    {:noreply, state}
-  end
-
-  @doc false
   def handle_cast({:insert, metadata}, state) do
-    state = state ++ [metadata]
+    state =
+      state
+      |> Map.put(:elements, state.elements ++ [{metadata, nil}])
+
     {:noreply, state}
+  end
+
+  @doc false
+  def handle_cast(:shuffle, state) do
+    size = length(state.elements) - 1
+
+    # Generating indexes for the new shuffle
+
+    shuffled_indexes =
+      0..(size)
+      |> Enum.to_list()
+      |> Enum.shuffle()
+
+    # Update all of the elements with a new shuffle index
+
+    new_elements =
+      0..(size)
+      |> Enum.map(&extract_reshuffled(&1, state, shuffled_indexes))
+
+    # Extract the new index of the current song playing
+
+    {_metadata, new_index} = Enum.at(new_elements, state.index)
+
+    # Set the current index to the new shuflfed one
+    # Set all of the elements to the new ones
+
+    state =
+      state
+      |> Map.put(:index, new_index)
+      |> Map.put(:elements, new_elements)
+      |> Map.put(:shuffle, true)
+
+    {:noreply, state}
+  end
+
+  def handle_cast(:unshuffle, state) do
+    current_meta_index =
+      state.elements
+      |> Enum.find_index(fn {_meta, index} -> index == state.index end)
+
+    state =
+      state
+      |> Map.put(:shuffle, false)
+      |> Map.put(:index, current_meta_index)
+
+    {:noreply, state}
+  end
+
+  defp extract_reshuffled(index, state, shuffled_indexes) do
+    {metadata, _index} = Enum.at(state.elements, index)
+    {metadata, Enum.at(shuffled_indexes, index)}
   end
 
   @doc false
   def start(guild_id, metadata) do
-    GenServer.start_link(__MODULE__, [metadata], name: from_guild_id(guild_id))
+    GenServer.start(__MODULE__, %Playlist{elements: [{metadata, nil}]}, name: from_guild_id(guild_id))
   end
 
   @doc false
@@ -113,5 +190,5 @@ defmodule Cassian.Servers.Playlist do
   end
 
   @doc false
-  defp from_guild_id(guild_id), do: "queue_#{guild_id}" |> String.to_atom()
+  defp from_guild_id(guild_id), do: "playlist_#{guild_id}" |> String.to_atom()
 end
--- lib/cassian/structs/playlist.ex
+++ lib/cassian/structs/playlist.ex
@@ -3,16 +3,19 @@ defmodule Cassian.Structs.Playlist do
   A struct representing a playlist and its' options.
   """
 
+  alias Cassian.Servers.Playlist
   alias Cassian.Structs.Metadata
 
   defstruct [
     shuffle: false,
-    elements: []
+    elements: [],
+    index: 0
   ]
 
   @type t() :: %__MODULE__{
           shuffle: boolean(),
-          elements: list({%Metadata{}, integer() | nil})
+          elements: list({%Metadata{}, integer() | nil}),
+          index: integer()
         }
 
   @typedoc "A boolean representing whether it is shuffling."
@@ -24,4 +27,15 @@ defmodule Cassian.Structs.Playlist do
   when shuffling.
   """
   @type elements :: list({%Metadata{}, integer()})
+
+  @typedoc """
+  The current index in the playlist.
+  """
+  @type index :: integer()
+
+  defdelegate exists?(guild_id), to: Playlist
+  defdelegate show(guild_id), to: Playlist
+  defdelegate insert!(guild_id, metadata), to: Playlist
+  defdelegate delete(guild_id), to: Playlist
+  defdelegate shuffle(guild_id), to: Playlist
 end
