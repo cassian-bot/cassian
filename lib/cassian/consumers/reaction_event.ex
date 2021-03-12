2c52994dfc89bb405174e65ccbfee23ec2206186
zastrixarundell <zastrixarundell@gmail.com>
2021-03-12 17:12:47 +0100

    Pattern-matching handle_emoji

--- lib/cassian/consumers/reaction_event.ex
+++ lib/cassian/consumers/reaction_event.ex
@@ -1,4 +1,6 @@
 defmodule Cassian.Consumers.ReactionEvent do
+  #alias Cassian.Structs.Playlist
+  require Logger
 
   @backwards "⬅️"
   @previous "⏮️"
@@ -12,49 +14,13 @@ defmodule Cassian.Consumers.ReactionEvent do
 
   @reactions [@backwards, @previous, @play_pause, @stop, @next, @shuffle, @repeat, @repeat_one, @forwards]
 
+  # Handlers as the consumers
+
   def handle_event(%{emoji: %{name: emoji}} = data) when emoji in @reactions do
     try do
       message = Nostrum.Api.get_channel_message!(data.channel_id, data.message_id)
-      if message.author.id == Cassian.own_id() do
-        case emoji do
-          @backwards ->
-            # Play backwards
-            IO.inspect("Backwards")
-
-          @previous ->
-            # Play previous
-            IO.inspect("Previous")
-
-          @play_pause ->
-            # Play or pause
-            IO.inspect("Play pause")
-
-          @stop ->
-            # Stop
-            IO.inspect("Stop")
-
-          @next ->
-            # Next
-            IO.inspect("Next")
-
-          @shuffle ->
-            # Shuffle
-            IO.inspect("Shuffle")
-
-          @repeat ->
-            # Repeat
-            IO.inspect("Repeat")
-
-          @repeat_one ->
-            # Repeat one
-            IO.inspect("Repeat one")
-
-          @forwards ->
-            # next
-            IO.inspect("Forwards")
-
-        end
-      end
+      if message.author.id == Cassian.own_id(),
+        do: handle_emoji(emoji, message)
     rescue
       Nostrum.Error.ApiError ->
         nil
@@ -64,4 +30,43 @@ defmodule Cassian.Consumers.ReactionEvent do
   def handle_event(_) do
     :noop
   end
+
+  # Emoji action handlers
+
+  defp handle_emoji(@backwards, _message) do
+    Logger.info("Playing backwards")
+  end
+
+  defp handle_emoji(@previous, _message) do
+    Logger.info("Playing previous")
+  end
+
+  defp handle_emoji(@play_pause, _message) do
+    Logger.info("Pllay/Pause")
+  end
+
+  defp handle_emoji(@stop, _message) do
+    Logger.info("Stopping music")
+  end
+
+  defp handle_emoji(@next, _message) do
+    Logger.info("Playing next")
+  end
+
+  defp handle_emoji(@shuffle, _message) do
+    Logger.info("Shufling / Disabling shuffle")
+  end
+
+  defp handle_emoji(@repeat, _message) do
+    Logger.info("Playing on repeat")
+  end
+
+  defp handle_emoji(@repeat_one, _message) do
+    Logger.info("Repeating one")
+  end
+
+  defp handle_emoji(@forwards, _message) do
+    Logger.info("Playing forwards")
+  end
+
 end
