604361f4713cd80ffb72202308614e744449cad8
zastrixarundell <zastrixarundell@gmail.com>
2021-03-13 15:27:25 +0100

    Different command behaviour

--- lib/cassian/behaviours/command.ex
+++ lib/cassian/behaviours/command.ex
@@ -5,29 +5,9 @@ defmodule Cassian.Behaviours.Command do
   """
 
   @doc """
-  Get the description of the command.
-  """
-  @callback desc() :: String.t()
-
-  @doc """
-  Get the caller for the command.
-  """
-  @callback caller() :: String.t()
-
-  @doc """
   The function which is called that does all of the stuff needed in the command.
   """
   @callback execute(message :: Nostrum.Struct.Message.t(), args :: List.t()) :: :ok
-
-  @doc """
-  Should the command be shipped in production?
-  """
-  @callback ship? :: boolean()
-
-  @doc """
-  A given example for a command in the help meny
-  """
-  @callback example :: String.t() | nil
 
   defmacro __using__(_) do
     quote do
