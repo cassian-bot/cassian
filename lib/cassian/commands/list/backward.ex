96028113416dd912fe57dc34955d53b071442e8c
zastrixarundell <zastrixarundell@gmail.com>
2021-03-13 13:37:15 +0100

    Using name list

--- lib/cassian/commands/list/backward.ex
+++ lib/cassian/commands/list/backward.ex
@@ -1,10 +1,10 @@
-defmodule Cassian.Commands.Playlist.Backward do
+defmodule Cassian.Commands.List.Backward do
   use Cassian.Behaviours.Command
 
   alias Cassian.Managers.PlayManager
 
   def ship?, do: true
-  def caller, do: "music:backward"
+  def caller, do: "list:backward"
   def desc, do: "Play the playlst in backward order!"
 
   def execute(message, _args) do
