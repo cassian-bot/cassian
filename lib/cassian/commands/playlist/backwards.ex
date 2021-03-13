5486c477b67c7bf468a6e3434cd14664a3448779
zastrixarundell <zastrixarundell@gmail.com>
2021-03-13 13:36:12 +0100

    Updated to backwards

--- lib/cassian/commands/playlist/backwards.ex
+++ lib/cassian/commands/playlist/backwards.ex
@@ -4,8 +4,8 @@ defmodule Cassian.Commands.Playlist.Backwards do
   alias Cassian.Managers.PlayManager
 
   def ship?, do: true
-  def caller, do: "music:reverse"
-  def desc, do: "Play the playlst in reverse order!"
+  def caller, do: "music:backwards"
+  def desc, do: "Play the playlst in backwards order!"
 
   def execute(message, _args) do
     PlayManager.change_direction_with_notification(message, true)
