8ee0946391eddfcba573099dc6473cb8dea9a083
zastrixarundell <zastrixarundell@gmail.com>
2021-03-13 13:36:43 +0100

    Using singular name

--- lib/cassian/commands/playlist/backward.ex
+++ lib/cassian/commands/playlist/backward.ex
@@ -1,11 +1,11 @@
-defmodule Cassian.Commands.Playlist.Backwards do
+defmodule Cassian.Commands.Playlist.Backward do
   use Cassian.Behaviours.Command
 
   alias Cassian.Managers.PlayManager
 
   def ship?, do: true
-  def caller, do: "music:backwards"
-  def desc, do: "Play the playlst in backwards order!"
+  def caller, do: "music:backward"
+  def desc, do: "Play the playlst in backward order!"
 
   def execute(message, _args) do
     PlayManager.change_direction_with_notification(message, true)
