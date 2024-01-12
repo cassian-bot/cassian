c6882309b663524317cd6a2509e19f0573e3ca72
zastrixarundell <luka@bodroza.com>
2024-01-12 22:57:25 +0100

    Minimize help function

--- lib/cassian/commands/bot/help.ex
+++ lib/cassian/commands/bot/help.ex
@@ -1,8 +1,6 @@
 defmodule Cassian.Commands.Bot.Help do
   use Cassian.Behaviours.Command
 
-  alias Cassian.Managers.MessageManager
-
   @moduledoc """
   The help command. Shows a menu of commands.
   """
@@ -14,24 +12,19 @@ defmodule Cassian.Commands.Bot.Help do
     }
   end
 
-  @doc false
-  def execute(interaction) do
-    generate_help_embed!()
-
+  def execute(_interaction) do
     %{type: 4, data: %{embeds: [generate_help_embed!()], flags: 1 <<< 6}}
   end
 
   import Cassian.Utils.Embed
   alias Nostrum.Struct.Embed
 
-  def generate_help_embed!() do
+  defp generate_help_embed!() do
     create_empty_embed!()
     |> Embed.put_title("Yo! Thanks for adding me to the server!")
     |> Embed.put_description(
       "I currently don't have a propper help command as I'm a WIP. Though once it has been created it will be linked here so " <>
-        "you can see all of my commands. Although you can start playing music with `#{
-          Cassian.command_prefix!()
-        }play`."
+        "you can see all of my commands. Although you can start playing music with `/play`."
     )
     |> Embed.put_thumbnail(Cassian.get_own_avatar())
   end
